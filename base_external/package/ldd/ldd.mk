91b0318c152deb14cb34e32d6098133d92142d24
taitbenj <taitbenj@Ubunru.myguest.virtualbox.org>
2023-10-22 22:25:59 -0400

    assgn7p2 changes

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,29 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-betai97.git
+
+$(eval LDD_VERSION=$(shell git ls-remote $(LDD_SITE) | grep HEAD | cut -f 1))
+
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = scull misc-modules
+
+
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
