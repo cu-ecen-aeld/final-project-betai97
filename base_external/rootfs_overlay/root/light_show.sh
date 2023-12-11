c62b5d26dc20318950ca4d86c38f2f30c6909c83
taitbenj <taitbenj@Ubunru.myguest.virtualbox.org>
2023-12-10 23:49:40 -0500

    added light show script

---
+++ base_external/rootfs_overlay/root/light_show.sh
@@ -0,0 +1,208 @@
+echo "echo Dream > /dev/aesdchar"
+echo Dream > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Hello world! > /dev/aesdchar"
+echo "Hello world!" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo It does not matter how slowly you go as long as you do not stop > /dev/aesdchar"
+echo "It does not matter how slowly you go as long as you do not stop" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo How much too much? > /dev/aesdchar"
+echo "How much too much?" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Remember that not getting what you want is sometimes a wonderful stroke of luck > dev/aesdchar"
+echo "Remember that not getting what you want is sometimes a wonderful stroke of luck" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Dream > /dev/aesdchar"
+echo Dream > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Hello world! > /dev/aesdchar"
+echo "Hello world!" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo String > /dev/aesdchar"
+echo "String" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Success is not final, failure is not fatal: It is the courage to continue that counts. > /dev/aesdchar"
+echo "Success is not final, failure is not fatal: It is the courage to continue that counts." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo The only limit to our realization of tomorrow will be our doubts of today. > /dev/aesdchar"
+echo "The only limit to our realization of tomorrow will be our doubts of today." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo The only way to do great work is to love what you do. > /dev/aesdchar"
+echo "The only way to do great work is to love what you do." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Elephant > /dev/aesdchar"
+echo "Elephant" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Sunshine > /dev/aesdchar"
+echo "Sunshine" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'In the middle of difficulty lies opportunity.' > /dev/aesdchar"
+echo "'In the middle of difficulty lies opportunity.'" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Adventure > /dev/aesdchar"
+echo "Adventure" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'Success is not final, failure is not fatal: It is the courage to continue that counts.' > /dev/aesdchar"
+echo "'Success is not final, failure is not fatal: It is the courage to continue that counts.'" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'The only way to do great work is to love what you do.' > /dev/aesdchar"
+echo "'The only way to do great work is to love what you do.'" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'The future belongs to those who believe in the beauty of their dreams.' > /dev/aesdchar"
+echo "The future belongs to those who believe in the beauty of their dreams." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Apple > /dev/aesdchar"
+echo "Apple" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'In three words I can sum up everything I've learned about life: it goes on.' > /dev/aesdchar"
+echo "In three words I can sum up everything I've learned about life: it goes on." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Wanderlust > /dev/aesdchar"
+echo "Wanderlust" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Sunshine > /dev/aesdchar"
+echo "Sunshine" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Mountain > /dev/aesdchar"
+echo "Mountain" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Ocean > /dev/aesdchar"
+echo "Ocean" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'The only limit to our realization of tomorrow will be our doubts of today.' > /dev/aesdchar"
+echo "The only limit to our realization of tomorrow will be our doubts of today." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'Life is what happens when you're busy making other plans.' > /dev/aesdchar"
+echo "Life is what happens when you're busy making other plans." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Archer > /dev/aesdchar"
+echo "Archer" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'The best way to predict the future is to create it.' > /dev/aesdchar"
+echo "The best way to predict the future is to create it." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Pig > /dev/aesdchar"
+echo "Pig" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Oink > /dev/aesdchar"
+echo "Oink" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo 'Challenges are what make life interesting and overcoming them is what makes life meaningful.' > /dev/aesdchar"
+echo "Challenges are what make life interesting and overcoming them is what makes life meaningful." > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Our lives begin to end the day we become silent about things that matter > /dev/aesdchar"
+echo "Our lives begin to end the day we become silent about things that matter" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Dream big and dare to fail > /dev/aesdchar"
+echo "Dream big and dare to fail" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Sprig > /dev/aesdchar"
+echo "Sprig" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Spring > /dev/aesdchar"
+echo "Spring" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Wealth > /dev/aesdchar"
+echo "Wealth" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo I have learned over the years that when one’s mind is made up, this diminishes fear > /dev/aesdchar"
+echo "I have learned over the years that when one’s mind is made up, this diminishes fear" > /dev/aesdchar
+
+sleep 0.3
+
+
+echo "echo It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live > /dev/aesdchar"
+echo "It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Jig > /dev/aesdchar"
+echo "Jig" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo echo > /dev/aesdchar"
+echo "echo" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Do what you can, where you are, with what you have > /dev/aesdchar"
+echo "Do what you can, where you are, with what you have" > /dev/aesdchar
+
+sleep 0.3
+
+echo "echo Filth > /dev/aesdchar"
+echo "Filth" > /dev/aesdchar
